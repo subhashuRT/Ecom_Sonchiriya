2f8613f0a62c25d0baf0bedf78345e4b058ee7eb
Subhanshu <68691037+subhashuRT@users.noreply.github.com>
2021-08-17 21:03:05 +0530

    aa

--- Vishal/shop/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-# Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('items','price', "stock","category","modified_date","is_available")
-    prepopulated_field = {"slug":("items",)}
-
-admin.site.register(Product,ProductAdmin)
